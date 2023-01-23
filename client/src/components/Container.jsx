099965b2338622f4ee33f886d6a9f5d73a1c2dcc
Babashov Mahammad <babashov.info@gmail.com>
2023-01-23 17:37:31 +0400

    initialized SocketProvider

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -1,7 +1,12 @@
 import ChatList from './ChatList'
 import ChatForm from './ChatForm'
+import {init} from '../providers/SocketProvider'
+import { useEffect } from 'react'
 
 function Container() {
+  useEffect(()=>{
+    init()
+  },[])
   return (
     <div>
         <ChatList/>
