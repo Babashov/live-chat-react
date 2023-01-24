0363b83bc563a3d3f650a3ceeec1984459632f9b
Babashov Mahammad <babashov.info@gmail.com>
2023-01-24 15:41:06 +0400

    sender's message add right side in the list

--- client/src/components/ChatItem.jsx
+++ client/src/components/ChatItem.jsx
@@ -2,7 +2,7 @@ import styles from './styles.module.css'
 
 function ChatItem({item}) {
   return (
-    <div className={styles.chatItem}>
+    <div className={`${styles.chatItem} ${item.byMe ? styles.right : ''}`}>
         {item.message}
     </div>
   )
