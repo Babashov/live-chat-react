e7cee6a0c30822ed8fca1bdc67035eb1b1e175c4
Babashov Mahammad <babashov.info@gmail.com>
2023-01-24 15:40:17 +0400

    Add the sender's message to the list

--- client/src/components/ChatForm.jsx
+++ client/src/components/ChatForm.jsx
@@ -1,13 +1,17 @@
 import styles from './styles.module.css'
 import {useState} from 'react'
 import { sendMessage } from '../providers/SocketProvider'
+import { useChat } from '../context/ChatContext'
 
 function ChatForm() {
   const [message,setMessage] = useState('')
+  const {setMessages} = useChat();
   const handleSubmit = (e)=>{
     e.preventDefault()
     console.log(message);
-    sendMessage(message)
+
+    setMessages((prevState)=>[...prevState,{message,byMe:true}])
+    sendMessage(message);
 
     setMessage('')
   }
