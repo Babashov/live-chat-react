de763eabd22c1a503e4afec664676f55a34ba308
Babashov Mahammad <babashov.info@gmail.com>
2023-01-24 15:14:15 +0400

    created sendMessage & subscribeChat methods

--- client/src/providers/SocketProvider.js
+++ client/src/providers/SocketProvider.js
@@ -12,3 +12,15 @@ export const init = ()=>{
     socket.on('connect',()=>console.log('Connected'))
 
 }
+
+export const sendMessage = (message)=>{
+    if(socket) socket.emit('new-message',message)
+}
+
+export const subscribeChat = ()=>{
+    if(!socket) return;
+
+    socket.on('receive-message',(message)=>{
+        console.log('Yeni mesaj var ',message);
+    })
+}
